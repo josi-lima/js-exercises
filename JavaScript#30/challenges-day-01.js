d2d0e1d48a719ca614084d253de38eeb21f4bfb3
josi-lima <teacher.josi@yahoo.com>
2022-09-29 16:19:49 -0300

    update js

---
+++ JavaScript#30/challenges-day-01.js
@@ -0,0 +1,91 @@
+// ================ Challenge JavaScript#30- Array Cardio #DAY-1 💪 ======================================
+
+// Some data to work with
+
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+
+const people = [
+  'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+  'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+  'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+  'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+  'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+];
+
+// ================================================================================
+
+// Array.prototype.filter()
+
+// 🚀 EXERCISE 1: Filter the list of inventors for those who were born in the 1500s.
+
+
+// =================================================================================
+
+// Array.prototype.map()
+
+// 🚀 EXERCISE 2: Give us an array of the inventors first and last names.
+
+
+
+// =================================================================================
+
+// Array.prototype.sort()
+
+// 🚀 EXERCISE 3: Sort the inventors by birthdate, oldest to youngest.
+
+
+
+// =================================================================================
+
+// Array.prototype.reduce()
+
+// 🚀 EXERCISE 4: How many years did the inventors live all together?
+
+
+// =================================================================================
+
+// Array.prototype.sort()
+
+// 🚀 EXERCISE 5: Sort the inventors by years lived, starting with the person who lived the most on the top.
+
+
+// =================================================================================
+
+// Array.prototype.includes()
+
+// 🚀 EXERCISE 6: Create a list of Boulevards in Paris that contain 'de' anywhere in the name.
+
+// Intructions: to test the code, go to this website, open the console and type the code there.
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+
+// =================================================================================
+
+// Array.prototype.sort()
+
+// 🚀 EXERCISE 7: Sort the people alphabetically by last names.
+
+
+
+// =================================================================================
+
+// Array.prototype.reduce()
+
+// 🚀 EXERCISE 8: // Sum up the instances of each of these items from the array.
+
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+
