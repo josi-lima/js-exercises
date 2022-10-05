1148e37c3f8ca9fcda62869aa3000b6eaca39e61
Josiane de Lima <108018406+josi-lima@users.noreply.github.com>
2022-10-05 10:38:25 -0300

    Update day-02-solutions.js

---
+++ JavaScript#30/day-02-solutions.js
@@ -0,0 +1,40 @@
+// ================ Challenge JavaScript#30- Array Cardio #DAY-2 💪 ======================================
+
+// Some data to work with
+
+const people = [
+      { name: 'Wes', year: 1988 },
+      { name: 'Kait', year: 1986 },
+      { name: 'Irv', year: 1970 },
+      { name: 'Lux', year: 2015 }
+    ];
+
+    const comments = [
+      { text: 'Love this!', id: 523423 },
+      { text: 'Super good', id: 823423 },
+      { text: 'You are the best', id: 2039842 },
+      { text: 'Ramen is my fav food ever', id: 123523 },
+      { text: 'Nice Nice Nice!', id: 542328 }
+    ];
+    
+    // ================================================================================
+    
+    // Array.prototype.some() 
+    // 🚀 EXERCISE 1: Check if at least one person is 19 or older.
+    
+          
+    // Array.prototype.every() // 
+    // 🚀 EXERCISE 2: Check if everyone is 19 or older.
+        
+
+    // Array.prototype.find() --> .find() is like .filter(), but instead it returns just the one element you are looking for.
+    // 🚀 EXERCISE 3: Find the comment with the ID of 823423.
+        
+
+    // Array.prototype.findIndex()
+    // 🚀 EXERCISE 4: Find the comment with this same ID (823423).
+    
+    
+     // Array.prototype.findIndex()
+    // 🚀 EXERCISE 5: Delete the comment with the ID of 823423.
+    
