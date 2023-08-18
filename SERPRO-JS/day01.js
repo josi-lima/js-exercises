9cc06a6198b03330c8362f5596bb1bf8f8008d60
josi-lima <teacher.josi@yahoo.com>
2023-08-18 09:35:48 -0300

    books: docs: add folder Serpro

---
+++ SERPRO-JS/day01.js
@@ -0,0 +1,100 @@
+// ================ SERPRO Training - day 01 ==============================
+
+// 🚀 1. Write a program to print "Hello, World!" 
+
+const greeting = () => console.log("Hello, World!");
+greeting();
+
+// ==================================================================
+
+// 🚀 2. Calculate the sum of two numbers entered by the user.
+
+const firstSum = (numA, numB) => {
+  return numA + numB;
+}
+console.log(firstSum(5,10));
+
+// ------------------------------
+
+// const secondSum = (numA, numB) => {
+//   numA = parseInt(prompt("Please, enter the first number: "));
+//   numB = parseInt(prompt("Please, enter the second number: "));
+//   return numA + numB;
+// }
+// console.log(secondSum())
+
+// ==================================================================
+
+// 🚀 3. Convert temperature from Celsius to Fahrenheit.
+// Reminder: C = F – 32 / 1.8, which means C * 1.8 + 32
+
+const convertCelsiusToFahr = (degCelsius) => {
+  degFahr = degCelsius * 1.8 + 32;
+  return degFahr;
+}
+console.log(convertCelsiusToFahr(23.4));
+
+// ==================================================================
+
+// 🚀 4. Write a program to calculate the area of a rectangle given its length and width.
+// The formula for the area, 'A' of a rectangle whose length and width are 'l' and 'w' respectively is the product of length and width, that is, "A = l × w".
+
+const calculateRecArea = (length, width) => {
+  area = length * width;
+  return area;
+}
+console.log(calculateRecArea(5.5, 7.0))
+
+// ==================================================================
+
+// 🚀 5. Create a program that takes a user's name and age as input and prints a greeting message.
+
+// const greetUser = () => {
+//   firstName = prompt("What's your first name? ");
+//   age = prompt("How old are you? ");
+//   console.log(`Welcome, ${firstName}! Today's your birthday! You're turning ${age}.`)
+// }
+// greetUser();
+
+
+// ==================================================================
+
+// 🚀 6. Write a program to check if a number is even or odd.
+
+const checkEvenOrOdd = (num) => num % 2 === 0 ? "even" : "odd";
+console.log(checkEvenOrOdd(17))
+
+// ==================================================================
+
+// 🚀 7. Given a list of numbers, find the maximum and minimum values.
+
+
+// ==================================================================
+
+// 🚀 8. Create a Python function to check if a given string is a palindrome.
+
+
+// ==================================================================
+
+// 🚀 9. Calculate the compound interest for a given amount, interest rate, and time period.
+
+
+// ==================================================================
+
+// 🚀 10. Write a program that converts a given number of days into years, weeks, and days.
+
+
+// ==================================================================
+
+// 🚀 11. Given a list of integers, find the sum of all positive numbers.
+
+
+// ==================================================================
+
+// 🚀 12. Create a program that takes a sentence as input and counts the number of words in it.
+
+
+// ==================================================================
+
+// 🚀 13. Implement a program that swaps the values of two variables.
+
