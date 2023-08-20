d689c200106216e8f07bb13f94c6a426f5c3c023
josi-lima <teacher.josi@yahoo.com>
2023-08-20 11:07:06 -0300

    :books: docs: add folder day04

---
+++ SERPRO-JS/day04.js
@@ -0,0 +1,42 @@
+// ================ SERPRO Training - day 04 ==============================
+
+
+// 🚀 1. Given a list of numbers, create a function to find the sum of all odd numbers.
+
+// ==================================================================
+
+// 🚀 2. Create a function to find the square of each element in a given list.
+
+// ==================================================================
+
+// 🚀 3. Implement a function that returns the factorial of a given number using recursion.
+
+// ==================================================================
+
+// 🚀 4. Calculate the area of a triangle given its base and height using a function. Base and height must be greater than 0.
+
+// ==================================================================
+
+// 🚀 5. Create a function that takes a list of strings and returns the list sorted alphabetically.
+
+// ==================================================================
+
+// 🚀 6. Write a function that takes two lists and returns their intersection (common elements).
+
+// ==================================================================
+
+// 🚀 7. Create a function that takes a number as input and prints its multiplication table.
+
+// ==================================================================
+
+// 🚀 8. Implement a function to check if a given year is a leap year or not.
+
+// ==================================================================
+
+// 🚀 9. Create a loop that prints all prime numbers between 1 and 50.
+
+// ==================================================================
+
+// 🚀 10. Create a program that generates the Fibonacci sequence up to a given number of terms.
+
+// # ==================================================================
